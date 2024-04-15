5b5f5fc70e005c29f84a3243b77374c9b942f456
PradeepaSundaram <pradeepasundaramnps@gmail.com>
2024-04-15 14:04:10 +0530

    Day-01

---
+++ book-my-show-clone-app/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+// React Slick CSS
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<HomePage/>} />
+      <Route path="/movie/:id" element={<MoviePage />} />
+      <Route path="/plays" element={<Plays />} /> */}
+    </Routes>
+  );
+}
+
+export default App;
